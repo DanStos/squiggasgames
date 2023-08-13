ec8f157211e2fdf41b2d0235d91d9dfa4a3c8402
Danilo Oliveira <oliveira.danilosantos@outlook.com>
2023-08-13 12:12:29 -0300

    fix: Corrected tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Testing username placeholder', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByPlaceholderText("Insira seu e-mail")
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Testing password placeholder', () => {
+  render(<App />);
+  const linkElement = screen.getByPlaceholderText("Insira sua senha")
   expect(linkElement).toBeInTheDocument();
 });
